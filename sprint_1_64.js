11274207cc8b44a278f2d2b5ecf1d279ad965f02
murillotlopes <murillo.tlopes@hotmail.com>
2021-08-09 09:14:58 -0300

    09-08-21 exercicio 'snapCrakle'

---
+++ sprint_1_64.js
@@ -0,0 +1,159 @@
+//console.log(snapCrackle(15))
+
+console.log(snapCracklePrime(15))
+
+
+
+function snapCracklePrime(maxValue){
+    let theReturn = ""
+    let word1 = " SnapCrackle"
+    let word2 = "Snap"
+    let word3 = "Crackle"
+    let word4 = "Prime"
+    let prime = []
+
+    prime = primos(maxValue)
+
+
+    for (let i=1; i<=maxValue; i++){
+        
+        if (i%2 !== 0 && i%5 === 0){
+            theReturn = theReturn + word1
+
+            for(let j=0; j<=i; j++){
+                if (i === prime[j]){
+                    theReturn = theReturn + word4
+                }
+            }
+            
+            if (i !== maxValue){
+                theReturn = theReturn + ", "
+            }
+
+        }else if (i%2 !== 0 && i%5 !== 0){
+            theReturn = theReturn + word2
+
+            for(let j=0; j<=i; j++){
+                if (i === prime[j]){
+                    theReturn = theReturn + word4
+                }
+            }
+
+            if (i !== maxValue){
+                theReturn = theReturn + ", "
+            }
+
+        }else if (i%5 === 0 && i%2 ===0){
+            theReturn = theReturn + word3
+
+            for(let j=0; j<=i; j++){
+                if (i === prime[j]){
+                    theReturn = theReturn + word4
+                }
+            }
+
+            if (i !== maxValue){
+                theReturn = theReturn + ", "
+            }
+
+        }else {
+                        
+            for(let j=0; j<=i; j++){
+                if (i === prime[j]){
+                    theReturn = theReturn + word4
+                    
+                }
+            }
+
+            if ((i !== (i%2 !== 0 && i%5 === 0)) && i!== (i%2 !== 0 && i%5 !== 0) && i !== (i%5 === 0 && i%2 ===0)){
+                for (let j=0; j<=i; i++){
+                    if (i !== prime[j]){
+                        theReturn = theReturn + i
+                    }
+                }
+                
+            }
+
+            if (i !== maxValue){
+                theReturn = theReturn + ", "
+            }
+
+        }
+        
+    }
+    return theReturn
+}
+
+
+function snapCrackle(maxValue){
+    let theReturn = ""
+    let word1 = " SnapCrackle"
+    let word2 = "Snap"
+    let word3 = "Crackle"
+
+
+    for (let i=1; i<=maxValue; i++){
+        
+        if (i%2 !== 0 && i%5 === 0){
+            theReturn = theReturn + word1
+            
+            if (i !== maxValue){
+                theReturn = theReturn + ", "
+            }
+
+        }else if (i%2 !== 0 && i%5 !== 0){
+            theReturn = theReturn + word2
+
+            if (i !== maxValue){
+                theReturn = theReturn + ", "
+            }
+
+        }else if (i%5 === 0 && i%2 ===0){
+            theReturn = theReturn + word3
+
+            if (i !== maxValue){
+                theReturn = theReturn + ", "
+            }
+
+        }else {
+            theReturn = theReturn + i
+
+            if (i !== maxValue){
+                theReturn = theReturn + ", "
+            }
+
+        }
+        
+    }
+    return theReturn
+}
+
+function primos(mxSeq){
+    let c=1, contDiv=0, n=1, n2=mxSeq, primos=[]
+
+do{
+    
+    do{
+
+        if (n%c ===0){
+            contDiv++
+        }
+    
+        c++
+
+    }while (c <= n)
+
+    if (contDiv !== 2){
+        
+    }else if (contDiv === 2){
+        primos.push(n)
+    }
+
+    n++
+    c=1
+    contDiv=0
+
+}while (n<n2)
+
+    return primos
+}
